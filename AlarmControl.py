10f281884bfee68fdba99390b1a64fa58ca65a70
Kadirhan Çekmez <56644475+kadirhancekmez@users.noreply.github.com>
2023-02-08 04:14:45 +0300

    Update AlarmControl.py

--- AlarmControl.py
+++ AlarmControl.py
@@ -1,24 +1,11 @@
- 
-
 def add_to_counter(x, ManagmentZone, counter):
-    if x["impactedEntities"][0]["entityId"]["type"] == "SERVICE":
+    if x["impactLevel"] == "SERVICES":
         counter[ManagmentZone]["fs"] += 1
-    elif x["impactedEntities"][0]["entityId"]["type"] == "HOST":
-        hostıd = (x["impactedEntities"][0]["entityId"]["id"])
-
-        responseHost = requests.get(
-        GeneralUrl + ENV + '/api/v2/entities/' + hostıd ,
-        headers=headers,
-        verify=False
-        )
-        HostInfo = json.loads(responseHost.text)
-        i = HostInfo["properties"]["monitoringMode"]
-        print(i)
-        if i == "FULL_STACK":
-                counter[ManagmentZone]["fs"] += 1
-        else:      
-                counter[ManagmentZone]["io"] += 1
-    else:
+    if x["impactLevel"] == "INFRASTRUCTURE":
+        counter[ManagmentZone]["io"] += 1
+    if x["impactLevel"] == "PROCESS_GROUP_INSTANCE":
+        counter[ManagmentZone]["io"] += 1
+    if x["impactLevel"] == "APPLICATION" :
         counter[ManagmentZone]["app"] += 1
     return counter
 
@@ -30,29 +17,31 @@ def Alarm(GeneralUrl,ENV,Token):
     params = {
     'pageSize': '500',
     }
-    responseALARM = requests.get(GeneralUrl + ENV + '/api/v2/problems', headers=headers,verify=False)
+    responseALARM = requests.get(GeneralUrl + ENV + '/api/v2/problems', headers=headers, params=params,verify=False)
     Alarm = json.loads(responseALARM.text)
 
     counter = {}
-
+    #print(Alarm["totalCount"])
     
     for x in Alarm["problems"]:
         #print(len(x["managementZones"]))
         #print(counter)
         if len(x["managementZones"]) != 0:
             if  x["managementZones"][0]["name"] not in counter: 
+                #print(x["managementZones"][0]["name"])
                 ManagmentZone = x["managementZones"][0]["name"]
-                counter[ManagmentZone] = {"io":0 , "fs":0 , "app":0 }
+                counter[ManagmentZone] = {"io":0 , "fs":0 , "app":0 }    
+                
             counter = add_to_counter(x, ManagmentZone, counter)
-        else:
+            
+            
+        else:    
             ManagmentZone = "YOK"
-            counter[ManagmentZone] = {"io":0 , "fs":0 , "app":0 }
-            counter = add_to_counter(x, ManagmentZone, counter)
-    
-    # create data frame from counter dictionary
+            if  ManagmentZone not in counter:   
+                counter[ManagmentZone] = {"io":0 , "fs":0 , "app":0 }
+            counter = add_to_counter(x, ManagmentZone,counter)
+            
     df = pd.DataFrame.from_dict(counter, orient='index')
-
-    # write data frame to excel file
     df.to_excel("counter.xlsx")
 
 Alarm(GeneralUrl,ProdEnv,ProdToken)  
